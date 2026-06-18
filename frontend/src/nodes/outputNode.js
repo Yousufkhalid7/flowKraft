9701322db9113fe64fb988e9d72e0672dc2fc641
Yousufkhalid7 <yousufkhalid1308@gmail.com>
2026-06-18 20:24:43 +0530

    Fix: Minor Issues

--- frontend/src/nodes/outputNode.js
+++ frontend/src/nodes/outputNode.js
@@ -1,17 +1,5 @@
 import { useState } from 'react';
 import { Position } from 'reactflow';
-import { BaseNode } from '/.BaseNode';
-
-export const OutputNode = ({ id, data }) => {
-  const [currName, setCurrName] = useState(data?.outputName || id.replace('customOutput-', 'output_'));
-  const [outputType, setOutputType] = useState(data.outputType || 'Text');
-
-  const handles = [
-    { type: 'target', position: Position.Left, id: '${id}-value'}
-  ];
-
-  return(import { useState } from 'react';
-import { Position } from 'reactflow';
 import { BaseNode } from './BaseNode';
 
 export const OutputNode = ({ id, data }) => {
@@ -57,5 +45,3 @@ export const OutputNode = ({ id, data }) => {
     </BaseNode>
   );
 };
-    <BaseNode title = "Output"
-  )
